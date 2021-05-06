d91626322130a78ac5248eb8d070dc60a3c9a1e4
NiceNilsson <gustav.nilsson@edu.nykoping.se>
2021-05-06 14:08:07 +0200

    adoliknoiua

--- Mallen/nav.js
+++ Mallen/nav.js
@@ -1,9 +1,18 @@
+document.getElementById("open").addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navSize = document.getElementById("TheSidebar").style.width;
+    if (navSize == 180) {
+        return closeNav();
+    }
+    return openNav();
+}
 function openNav() {
     document.getElementById("TheSidebar").style.width ="180px";
-    //document.getElementById("main").style.marginLeft ="180px"
 }
 
 function closeNav() {
     document.getElementById("TheSidebar").style.width ="0";
-    //document.getElementById("main").style.marginLeft ="0"
 }
+
+
