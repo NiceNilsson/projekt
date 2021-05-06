0d22e36589eb6b7727d59aab04f92be8e629c987
NiceNilsson <gustav.nilsson@edu.nykoping.se>
2021-05-06 14:24:24 +0200

    ackljnaojki

--- Mallen/nav.js
+++ Mallen/nav.js
@@ -1,18 +1,26 @@
-document.getElementById("open").addEventListener("click", toggleNav);
 
-function toggleNav(){
+
+/*function toggleNav(){
     navSize = document.getElementById("TheSidebar").style.width;
     if (navSize == 180) {
         return closeNav();
     }
     return openNav();
-}
+}*/
+
+
 function openNav() {
-    document.getElementById("TheSidebar").style.width ="180px";
+    var e = document.getElementById("TheSidebar");
+    if (e.style.width == "180px")   
+    {
+            e.style.width = "0px";
+    } 
+    else
+    {
+        e.style.width = "180px";
+    }
 }
 
 function closeNav() {
     document.getElementById("TheSidebar").style.width ="0";
 }
-
-
