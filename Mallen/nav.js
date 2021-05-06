779ff34ebfe7cf5322098078514489cd7b777fd7
NiceNilsson <gustav.nilsson@edu.nykoping.se>
2021-05-06 09:52:32 +0200

    lkascnlkas

--- Mallen/nav.js
+++ Mallen/nav.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("TheSidebar").style.width ="180px";
-    document.getElementById("main").style.marginLeft ="180px"
+    //document.getElementById("main").style.marginLeft ="180px"
 }
 
 function closeNav() {
     document.getElementById("TheSidebar").style.width ="0";
-    document.getElementById("main").style.marginLeft ="0"
+    //document.getElementById("main").style.marginLeft ="0"
 }
